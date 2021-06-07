f75767f022f9e2af31f15cfc377b849036353235
suxiongwei <suxiongwei@smzdm.com>
2021-06-07 09:48:45 +0800

    feat: 测试SqlSession的构建

--- src/test/java/com/sxw/MyBatisQuickStart.java
+++ src/test/java/com/sxw/MyBatisQuickStart.java
@@ -2,16 +2,15 @@ package com.sxw;
 
 import com.sxw.entity.TUser;
 import com.sxw.mapper.TUserMapper;
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.ExecutorType;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.session.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 /**
  * @author 苏雄伟 [suxiongwei@kaoshixing.com]
@@ -40,6 +39,23 @@ public class MyBatisQuickStart {
         // 4.执行查询语句并返回结果
         TUser user = userMapper.selectByPrimaryKey(1);
         System.out.println(user.toString());
-
     }
+
+    @Test
+    public void testConfiguration() throws IOException {
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        // 创建XMLConfigBuilder
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(reader);
+        // 调用parse方法 解析xml创建configuration对象
+        Configuration configuration = xmlConfigBuilder.parse();
+    }
+
+    @Test
+    public void testSqlSession() throws IOException {
+        // MyBatis主配置文件输入流
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+    }
+
 }
